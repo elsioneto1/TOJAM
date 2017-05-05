86bdf9c4625f344d8d271b0bfa928d35d214f536
LAPTOP-N58UJDKO\Elsio <elsioneto@gmail.com>
2017-05-05 17:43:32 -0400

    CAMARAO QUE DORME A ONDA LEVA HU3H3UH3U3H

---
+++ Assets/Scripts/Inputs/InputParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputParser : MonoBehaviour
+{
+
+
+    public static float GetHorizontal()
+    {
+        float returnValue = 0;
+
+        returnValue = Input.GetAxis("Horizontal");
+
+        return returnValue;
+
+    }
+
+
+    public static float GetVertical()
+    {
+
+        float returnValue = 0;
+
+        returnValue = Input.GetAxis("Vertical");
+
+        return returnValue;
+
+
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+
+    float InputX;
+    float InputY;
+    public float speed;
+    Vector3 translate;
+
+    public GameObject ObjectReference;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        InputX = InputParser.GetHorizontal();
+        InputY = InputParser.GetVertical();
+        // input gatherer
+        translate = new Vector3(InputX, 0, InputY);
+    }
+
+    private void FixedUpdate()
+    {
+
+        
+
+        // move the fockn character
+        transform.position += (translate * Time.fixedDeltaTime * speed);
+
+        if (translate != Vector3.zero)
+        transform.forward = ( translate.normalized);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, translate, Color.blue);
+    }
+
+}
